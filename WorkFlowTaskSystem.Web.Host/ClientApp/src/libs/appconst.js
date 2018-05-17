d7c7172ef850e3fd0e9d9a029056ea4bd7db66ef
chenbin <1247954091@qq.com>
2018-05-17 17:22:20 +0800

    提交

---
+++ WorkFlowTaskSystem.Web.Host/ClientApp/src/libs/appconst.js
@@ -0,0 +1,15 @@
+const AppConsts = {
+    userManagement: {
+        defaultAdminUserName: 'admin'
+    },
+    localization: {
+        defaultLocalizationSourceName: 'MyFirstVueABP'
+    },
+    authorization: {
+        encrptedAuthTokenName: 'enc_auth_token'
+    },
+    appBaseUrl: "http://localhost:8888",
+    remoteServiceBaseUrl:"http://localhost:8056"
+    //remoteServiceBaseUrl:"http://localhost:21021"
+}
+export default AppConsts
