6a60e63e787440c82e447ef621ba35462c1ae996
chenbin <1247954091@qq.com>
2018-05-17 17:11:01 +0800

    解决循环依赖

---
+++ WorkFlowTaskSystem.Web.Host/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkFlowTaskSystem.Controllers;
+using WorkFlowTaskSystem.Web.Host.Models;
+
+namespace WorkFlowTaskSystem.Web.Host.Controllers
+{
+    public class HomeController : WorkFlowTaskSystemControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
